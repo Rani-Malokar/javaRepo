be0f9d21c95ec108becce9803b637aee4691b0ed
snehagit <sneha@gmail.com>
2022-09-05 17:55:13 +0530

    initial commit

---
+++ src/main/java/TestJava.java
@@ -0,0 +1,16 @@
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestJava {
+
+
+   @Test
+   public void test1()
+   {
+      System.out.println("Running First Test");
+      Assert.assertEquals("Sneha","Sneha");
+   }
+
+
+}
